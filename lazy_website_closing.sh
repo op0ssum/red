42684f8db2f29454a1d09018149222b34fbf586e
op0ssum <92015617+op0ssum@users.noreply.github.com>
2024-02-21 13:16:00 +0800

    Create lazy_website_closing.sh

---
+++ lazy_website_closing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# run in webroot - cred: https://gist.github.com/Mr-Un1k0d3r/11bf902555d401c92c2e1b766275e6a2
+echo "Cloning $1"
+wget $1 -O index.html &> /dev/null
+TAG="<base href=\"$1\"/></head>"
+sed '/<\/head>/i\'"$TAG" index.html | tee index.html &> /dev/null
+echo "index.html was saved and modified"
