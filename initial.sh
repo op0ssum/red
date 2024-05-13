b5124872813f731767eb059ea4544920f22228df
op0ssum <92015617+op0ssum@users.noreply.github.com>
2024-05-13 12:21:15 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -13,7 +13,7 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | 
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get -y update
 echo "[+] installing essentials.."
-sudo apt-get -y install apt-transport-https awscli ca-certificates checksec dirmngr ghidra git gnupg jq krb5-user pipx samba sublime-text seclists  
+sudo apt-get -y install apt-transport-https awscli ca-certificates checksec dirmngr gdb ghidra git gnupg jq krb5-user pipx samba sublime-text seclists  
 echo "[+] setting up samba.." 
 sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.old
 cat <<EOF >>/etc/samba/smb.conf
